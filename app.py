8588a39e9bfb4aef4de16e5853a6a336b7fb9cd9
devenes <ahmedenesturan@gmail.com>
2022-01-31 00:46:37 +0300

    Update files

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request
 # Create an object named app
 app = Flask(__name__)
 
+
 # Create name and lastname variables for main page
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('main.html', name='Enes', lastname='Turan')
+
 
 # Add a statement to run the Flask application which can be reached from any host on port 80
 if __name__ == '__main__':
