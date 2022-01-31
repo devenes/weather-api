d533d563658e9dfe772cfeccedafbd85e90d43ba
devenes <ahmedenesturan@gmail.com>
2022-01-31 15:57:52 +0300

    Update API

--- app/weather-app.py
+++ app/weather-app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
 # City Name
-CITY = "İzmir"
+CITY = "izmir"
 
 # Your API key
 API_KEY = "99fd063b248c10d275944af7307bc5a6"
@@ -34,7 +34,7 @@ if response.status_code == 200:
     # getting temperature
     temperature = main['temp']
 
-    print(f"{CITY:-^35}")
+    # print(f"{CITY:-^35}")
     print(f"Temperature: {temperature}")
 else:
     # showing the error message
@@ -45,20 +45,34 @@ else:
 @app.route('/', methods=['GET', 'POST'])
 def home():
     # return render_template('main.html', name='Enes', lastname='Turan')
-    name = "Enes"
-    lastname = "Turan"
-    return f'{name} {lastname}'
+    # name = "Enes"
+    # lastname = "Turan"
+    # return f'{name} {lastname}'
+    return '{"name":"Enes", "lastname":"Turan"}'
 
 
 @app.route('/temperature', methods=['GET', 'POST'])
 def endpoint():
-    return render_template('temperature.html')
+    CITY = request.args.get('city')
+    URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+    weather_api_aldisi = requests.get(URL)
+    if weather_api_aldisi.status_code == 200:
+        veri = weather_api_aldisi.json()
+        main = veri['main']
+        sicaklik = main['temp']
+        sicaklik = (sicaklik - 273.15)
+        # return f'{temperature}'
+    # Sıcaklık değeri virgül sonrası 2 basamaklı hale çevrildikten sonra string olarak döndürüldü.
+    return '{"temperature":' + str(round(sicaklik, 2)) + '}'
+    # return render_template('temperature.html')
 
 
-@app.route('/temperature/<string:city>', methods=['GET', 'POST'])
+@ app.route('/temperature/<string:city>', methods=['GET'])
 def city(city):
+    CITY = request.args.get('city')
     # return render_template('temperature_city.html', city=city)
     return f'{city}: {temperature}'
+    # return '{"{{city}}":"{{temperature}}"}'
 
 
 # Add a statement to run the Flask application which can be reached from any host on port 80
