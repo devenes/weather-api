f49c3903436df89e288b9d754d44579d4c4a0e0d
devenes <ahmedenesturan@gmail.com>
2022-01-31 03:39:59 +0300

    Update weather app

--- app.py
+++ app.py
@@ -1,19 +1,51 @@
-# importing flask, render_template, requests and json
+# importing flask, render_template, requests and json modules
 import requests
+import json
 from flask import Flask, render_template, request, json, redirect, url_for
 
 # Create an object named app
 app = Flask(__name__)
 
-# Create name and lastname variables for the fonction
-name = "Enes"
-lastname = "Turan"
+# API base URL
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+# City Name
+city = "izmir"
+
+# Your API key
+API_KEY = "99fd063b248c10d275944af7307bc5a6"
+
+# updating the URL
+URL = BASE_URL + "q=" + city + "&appid=" + API_KEY
+
+# Sending HTTP request
+response = requests.get(URL)
+
+# checking the status code of the request
+if response.status_code == 200:
+
+    # retrieving data in the json format
+    data = response.json()
+
+    # take the main dict block
+    main = data['main']
+
+    # getting temperature
+    temperature = main['temp']
+
+    print(f"{city:-^35}")
+    print(f"Temperature: {temperature}")
+else:
+    # showing the error message
+    print("Error in the HTTP request")
 
 
 # Create name and lastname variables for main page
 @app.route('/', methods=['GET', 'POST'])
 def home():
     # return render_template('main.html', name='Enes', lastname='Turan')
+    name = "Enes"
+    lastname = "Turan"
     return f'{name} {lastname}'
 
 
@@ -24,9 +56,8 @@ def temperature():
 
 @app.route('/temperature/<string:city>', methods=['GET', 'POST'])
 def city(city):
-    return f'{city}'
-
     # return render_template('temperature_city.html', city=city)
+    return f'{city}: {temperature}'
 
 
 # Add a statement to run the Flask application which can be reached from any host on port 80
