d67c2376854a9b3989f77e30a2814e4b846199b8
devenes <ahmedenesturan@gmail.com>
2022-01-31 18:24:22 +0300

    Update API

--- app/weather-app.py
+++ app/weather-app.py
@@ -41,16 +41,13 @@ else:
     print("Error in the HTTP request")
 
 
-# Create name and lastname variables for main page
+# Create name and lastname json for main page
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    # return render_template('main.html', name='Enes', lastname='Turan')
-    # name = "Enes"
-    # lastname = "Turan"
-    # return f'{name} {lastname}'
     return '{"name":"Enes", "lastname":"Turan"}'
 
 
+# Create a route for the API page
 @app.route('/temperature', methods=['GET'])
 def endpoint():
     CITY = request.args.get('city')
@@ -60,19 +57,10 @@ def endpoint():
         veri = weather_api_aldisi.json()
         main = veri['main']
         sicaklik = main['temp']
+        # Kelvin to Celsius conversion
         sicaklik = (sicaklik - 273.15)
-        # return f'{temperature}'
-    # Sıcaklık değeri virgül sonrası 2 basamaklı hale çevrildikten sonra string olarak döndürüldü.
+    # Sıcaklık değeri virgül sonrası 2 basamaklı hale çevrildikten sonra string olarak döndürüldü
     return '{"temperature":' + str(round(sicaklik, 2)) + '}'
-    # return render_template('temperature.html')
-
-
-# @ app.route('/temperature/<string:city>', methods=['GET'])
-# def city(city):
-#     CITY = request.args.get('city')
-    # return render_template('temperature_city.html', city=city)
-    # return f'{city}: {temperature}'
-    # return '{"{{city}}":"{{temperature}}"}'
 
 
 # Add a statement to run the Flask application which can be reached from any host on port 80
